ece2be41812cc363cfb8c5754b3aa209535825d4
amirbaghi <bamirmasoud@gmail.com>
2021-07-14 21:52:33 +0430

    add user and user repo

--- api/domain/repository/UserRespository.go
+++ api/domain/repository/UserRespository.go
@@ -5,8 +5,5 @@ import (
 )
 
 type UserRepository interface {
-	SaveUser(*entity.User) (*entity.User, map[string]string)
-	GetUser(uint64) (*entity.User, error)
-	GetUsers() ([]entity.User, error)
-	GetUserByEmailAndPassword(*entity.User) (*entity.User, map[string]string)
+	SaveUser(user *entity.User) error
 }
---
+++ api/infrastructure/persistance/UserRepository.go
@@ -0,0 +1,28 @@
+package persistance
+
+import (
+	"github.com/Atelier-Developers/alethia/Database"
+	"github.com/Atelier-Developers/alethia/domain/entity"
+	"log"
+)
+
+type UserRepository struct {
+	dbClient *Database.MySQLDB
+}
+
+func (userRepo *UserRepository) SaveUser(user *entity.User) error {
+	db := userRepo.dbClient.GetDB()
+	stmt, err := db.Prepare("INSERT INTO USER (first_name, last_name, username, password, intro, about, accomplishments, additional_information, join_date, birthdate) VALUES  (?, ?, ?, ?, ?, ?, ?, ?, ?, ?)")
+	if err != nil {
+		log.Fatal(err)
+	}
+	defer stmt.Close()
+
+	_, err = stmt.Exec(user.FirstName, user.LastName, user.Username, user.Password, user.Intro, user.About, user.Accomplishments, user.AdditionalInfo, user.JoinDate, user.BirthDate)
+
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	return nil
+}
