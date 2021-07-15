b069af99b8479e1cb7a1ea64acd6bcefa1be420a
Amir Mohammad <tavakkoli.amirmohammad@gmail.com>
2021-07-15 19:15:57 +0430

    Fix docker file of front app to work with vue router and add get comment notifications

--- api/infrastructure/persistance/NotificationRepository.go
+++ api/infrastructure/persistance/NotificationRepository.go
@@ -55,18 +55,13 @@ func (notificationRepository *NotificationRepository) getNotification(notificati
 
 func (notificationRepository *NotificationRepository) CreateCommentNotification(comment *notification.Comment) error {
 	db := notificationRepository.dbClient.GetDB()
-	stmt, err := db.Prepare("SELECT * FROM NOTIFICATION_COMMENT")
-	if err != nil {
-		log.Fatal(err)
-	}
-	defer stmt.Close()
-	err = notificationRepository.creatNotification(&comment.Notification)
+	err := notificationRepository.creatNotification(&comment.Notification)
 
 	if err != nil {
 		log.Fatal(err)
 	}
 
-	stmt, err = db.Prepare("INSERT INTO NOTIFICATION_COMMENT (notif_id, comment_id) VALUES (?, ?)")
+	stmt, err := db.Prepare("INSERT INTO NOTIFICATION_COMMENT (notif_id, comment_id) VALUES (?, ?)")
 	if err != nil {
 		log.Fatal(err)
 	}
@@ -80,24 +75,24 @@ func (notificationRepository *NotificationRepository) CreateCommentNotification(
 	return nil
 }
 
-func (notificationRepository *NotificationRepository) GetCommentNotification(comment *notification.Comment) error {
-	//db := notificationRepository.dbClient.GetDB()
-	//notif, err := notificationRepository.getNotification(&comment.Notification)
-	//
-	//if err != nil {
-	//	log.Fatal(err)
-	//}
-	//
-	//stmt, err := db.Prepare("INSERT INTO NOTIFICATION_COMMENT (notif_id, comment_id) VALUES (?, ?)")
-	//if err != nil {
-	//	log.Fatal(err)
-	//}
-	//defer stmt.Close()
-	//
-	//_, err = stmt.Exec(comment.Notification.Id, comment.CommentId)
-	//
-	//if err != nil {
-	//	log.Fatal(err)
-	//}
-	return nil
+func (notificationRepository *NotificationRepository) GetCommentNotification() ([]notification.Comment, error) {
+	db := notificationRepository.dbClient.GetDB()
+	var notifs []notification.Comment
+	rows, err := db.Query("SELECT * FROM NOTIFICATION_COMMENT")
+	if err != nil {
+		log.Fatal(err)
+	}
+	for rows.Next() {
+		var nc notification.Comment
+		err := rows.Scan(&nc.Id, &nc.CommentId)
+		if err != nil {
+			log.Fatal(err)
+		}
+		nc.Notification, err = notificationRepository.getNotification(nc.Id)
+		if err != nil {
+			log.Fatal(err)
+		}
+		notifs = append(notifs, nc)
+	}
+	return notifs, nil
 }
