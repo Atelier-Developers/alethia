5fe088a02c85a5c789d82f94ba8b29e71d19c177
amirbaghi <bamirmasoud@gmail.com>
2021-07-17 03:22:01 +0430

    return name as well as id when getting user friends and invites

--- api/domain/entity/Invite.go
+++ api/domain/entity/Invite.go
@@ -3,8 +3,10 @@ package entity
 import "time"
 
 type Invite struct {
-	UserId1 uint64    `json:"user_id_1"`
-	UserId2 uint64    `json:"user_id_2"`
-	Created time.Time `json:"created"`
-	Body    string    `json:"body"`
+	UserId1   uint64    `json:"user_id_1"`
+	Username1 string    `json:"username_1"`
+	UserId2   uint64    `json:"user_id_2"`
+	Username2 string    `json:"username_2"`
+	Created   time.Time `json:"created"`
+	Body      string    `json:"body"`
 }
