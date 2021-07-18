c5fa3ce5b176d990cec72fd88e14dc4ef103fa74
Amir Mohammad <tavakkoli.amirmohammad@gmail.com>
2021-07-18 20:22:46 +0430

    Refactor invice friend vuex module

--- front/src/store/modules/invite-friend-module/index.js
+++ front/src/store/modules/invite-friend-module/index.js
@@ -18,65 +18,28 @@ const mutations = {
 
 const actions = {
     async getFriends(context,) {
-        console.log("GET FRIENDS");
-        try {
-            let res = await axios.get(API.getFriends);
-            console.log(res);
-            context.commit('setFriends', res.data);
-        } catch (e) {
-            console.log(e);
-        }
+        let res = await axios.get(API.getFriends);
+        context.commit('setFriends', res.data);
     },
     async getInvites(context,) {
-        console.log("GET Invites");
-        try {
-            let res = await axios.get(API.getInvites);
-            console.log(res);
-            context.commit('setInvites', res.data);
-        } catch (e) {
-            console.log(e);
-        }
+        const res = await axios.get(API.getInvites);
+        context.commit('setInvites', res.data);
     },
     async createInvite(context, payload) {
-        console.log("Create Invites");
-        try {
-            let res = await axios.post(API.getInvites, payload);
-            console.log(res);
-        } catch (e) {
-            console.log(e);
-        }
+        await axios.post(API.getInvites, payload);
     },
     async deleteInvite(context, payload) {
-        console.log("Delete Invites");
-        console.log(payload)
-        try {
-            let res = await axios.delete(API.getInvites, {
-                data: payload
-            });
-            console.log(res);
-        } catch (e) {
-            console.log(e);
-        }
+        await axios.delete(API.getInvites, {
+            data: payload
+        });
     },
     async addFriend(context, payload) {
-        console.log("add Friend");
-        try {
-            let res = await axios.post(API.getFriends, payload);
-            console.log(res);
-        } catch (e) {
-            console.log(e);
-        }
+        await axios.post(API.getFriends, payload);
     },
     async deleteFriend(context, payload) {
-        console.log("Delete Friend");
-        try {
-            let res = await axios.delete(API.getFriends,  {
-                data: payload
-            });
-            console.log(res);
-        } catch (e) {
-            console.log(e);
-        }
+        await axios.delete(API.getFriends, {
+            data: payload
+        });
     },
 };
 
